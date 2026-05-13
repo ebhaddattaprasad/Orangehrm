b7911e3f22f811984cf43eb6d9c5f46bbfe01719
ebhaddattaprasad <ebhaddattaprasad@gmail.com>
2026-05-13 20:21:43 +0530

    New cases added

---
+++ python programs/9_duplicate.py
@@ -0,0 +1,9 @@
+s="This is a duplicate word in the sentence. This will help to find the duplicate words in the sentence."
+y=s.split()
+d={}
+for i in y:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] = 1
+print(d)
